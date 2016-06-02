3338517a02626cf53527708cdc6ddb7ff2bf88a9
Joris Brauns <Joris Brauns>
2016-06-02 20:26:28 +0200

    Fix template, Fix weather demo allow API call on android, Update plugin template

---
+++ cordova-plugin-template/src/windows/HelloProxy.js
@@ -0,0 +1,10 @@
+﻿cordova.commandProxy.add("Hello", {
+    greet: function (successCallback, errorCallback, strInput) {
+        if (!strInput || !strInput.length) {
+            errorCallback("Error, something was wrong with the input string. =>" + strInput);
+        }
+        else {
+            successCallback(Hello.Hello.greet(strInput));
+        }
+    }
+});
